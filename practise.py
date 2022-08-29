b66ee655d6ab02c3b4f2411a97463282f6a27534
Subhan Shaik <subhan_shaik@persistent.com>
2022-08-29 15:32:57 +0530

    Added one more file

---
+++ practise.py
@@ -0,0 +1,8 @@
+words = "what is this how are you"
+frequency_of_words = {}
+for word in words.split():
+    if word not in frequency_of_words:
+        frequency_of_words[word] = 1
+    else:
+        frequency_of_words[word] += 1
+print(frequency_of_words)
